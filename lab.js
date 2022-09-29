7039420309f6069b38e4b7fb15390d17ec185b19
Donovan Tong <donotong01@gmail.com>
2022-09-28 20:23:00 -0600

    Problems 1 through 8

--- lab.js
+++ lab.js
@@ -7,7 +7,12 @@
 */
 
 //CODE HERE
-
+let lovesCode = true
+if (lovesCode) {
+  console.log("I love to code!")
+} else {
+  console.log("Coding has its challenges")
+}
 // For problems 2-3 use the following lines of code:
 var amysAge = 29;
 var brittanisAge = 34;
@@ -21,7 +26,13 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
-
+if (amysAge > brittanisAge) {
+  console.log("Amy is older")
+} else if (brittanisAge > amysAge) {
+  console.log("Brittani is older")
+} else if (brittanisAge === amysAge) {
+  console.log("They are the same age")
+}
 ////////// PROBLEM 3 //////////
 
 /*
@@ -29,7 +40,11 @@ var brittanisBirthYear = 1986;
 */
 
 //CODE HERE
-
+if (amysBirthYear === brittanisBirthYear) {
+  console.log("Amy & Brittani were born in the same year")
+} else {
+  console.log("Amy & Brittani were not born in the same year")
+}
 ////////// PROBLEM 4 //////////
 
 let temperature = 55;
@@ -41,7 +56,15 @@ let rain = true;
 */
 
 //CODE HERE
-
+if (temperature >= 80 && rain === true) {
+  console.log("One should wear a t-shirt and take an umbrella.")
+} else if (temperature <= 80 && temperature >= 60 && rain === true) {
+  console.log("One should wear a rain-jacket.")
+} else if (temperature <= 60) {
+  console.log("One should wear a jacket and carry an umbrella")
+} else if (rain != true) {
+  console.log("One should wear a t-shirt and slippers.")
+}
 ////////// PROBLEM 5 //////////
 
 /*
@@ -49,7 +72,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (i = 0; i<10; i++) {
+  console.log("hello")
+}
 ////////// PROBLEM 6 //////////
 
 /*
@@ -57,7 +82,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (i = 0; i<10; i++) {
+  console.log(i+1)
+}
 ////////// PROBLEM 7 //////////
 
 /*
@@ -65,7 +92,9 @@ let rain = true;
 */
 
 //CODE HERE
-
+for (i=10; i>0; i--) {
+  console.log(i)
+}
 
 ////////// PROBLEM 8 //////////
 
@@ -77,7 +106,12 @@ let passingScore = 7;
 */
 
 //CODE HERE
-
+while (score<7) {
+  for (i; i<=7; i++)
+  if (score != 7) {
+    console.log("Your score is not high enough")
+  }
+}
 ////////// INTERMEDIATE PROBLEMS //////////
 
 ////////// PROBLEM 9 //////////
